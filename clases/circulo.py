ac12712a10a3e1867f7b3e628b4e596b159b072b
Donijimg <doncanjimenez5@gmail.com>
2023-11-27 19:32:57 -0500

    actualizacion

---
+++ clases/circulo.py
@@ -0,0 +1,8 @@
+class Circulo:
+	def __init__(self,radio):
+		self.radio=radio
+		self.PI=3.14
+	def area(self):
+		return f"circulo: {PI*radio**2}"
+	def perimetro(self):
+		return f"circulo: {PI*radio*2}"
