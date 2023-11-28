dcf2a6438e7bf07e16f63cccfc9d7f14016cf226
Donijimg <doncanjimenez5@gmail.com>
2023-11-27 21:15:03 -0500

    actualizacion

--- clases/instancias.py
+++ clases/instancias.py
@@ -1,4 +1,8 @@
 from cuadrado import Cuadrado
+from rectangulo import Rectangulo
+from circulo import Circulo
+from triangulo import Triangulo
+from hexagono import Hexagono
 
 
 cuadro = Cuadrado(2)
@@ -9,4 +13,20 @@ print (f"Lado actual: {cuadro.lado}")
 cuadro.lado = 4
 print (f"Nuevo lado: {cuadro.lado}\n")
 
+circulo1=Circulo(9)
+print (circulo1.perimetro())
+print (circulo1.area())
+
+hexagono1=Hexagono(9)
+print (hexagono1.perimetro())
+print (hexagono1.area())
+
+triangulo1=Triangulo(9,3,3,2,2)
+print (triangulo1.perimetro())
+print (triangulo1.area())
+
+
+rectangulo1=Rectangulo(2,1)
+print (rectangulo1.perimetro())
+print (rectangulo1.area())
 
